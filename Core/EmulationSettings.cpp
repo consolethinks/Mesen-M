a9ddcf73488e676b10c121f2d144fb4312c0a9af
console <console@DESKTOP-J79UBT4>
2021-02-28 17:50:06 +0000

    Update version number

--- Core/EmulationSettings.cpp
+++ Core/EmulationSettings.cpp
@@ -3,10 +3,10 @@
 #include "Console.h"
 #include "RewindManager.h"
 
-//Version 0.9.9
+//Version 0.10.0
 uint16_t EmulationSettings::_versionMajor = 0;
-uint8_t EmulationSettings::_versionMinor = 9;
-uint8_t EmulationSettings::_versionRevision = 9;
+uint8_t EmulationSettings::_versionMinor = 10;
+uint8_t EmulationSettings::_versionRevision = 0;
 
 SimpleLock EmulationSettings::_lock;
 SimpleLock EmulationSettings::_shortcutLock;
--- GUI.NET/Properties/AssemblyInfo.cs
+++ GUI.NET/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mesen (Beta)")]
+[assembly: AssemblyTitle("Mesen-M (Beta)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Mesen")]
-[assembly: AssemblyProduct("Mesen (Beta)")]
-[assembly: AssemblyCopyright("Copyright © 2020 M. Bibaud")]
+[assembly: AssemblyCompany("Mesen-M")]
+[assembly: AssemblyProduct("Mesen-M (Beta)")]
+[assembly: AssemblyCopyright("Copyright © 2021 Mesen-M devs")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.*")]
-[assembly: AssemblyFileVersion("0.9.9.0")]
+[assembly: AssemblyVersion("0.10.*")]
+[assembly: AssemblyFileVersion("0.10.0.0")]
